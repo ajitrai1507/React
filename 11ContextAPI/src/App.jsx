c39e129eabeb3171617196f6610356eceb1604b1
Ajit Rai <ajitrai0715@gmail.com>
2024-09-25 00:38:28 +0530

    apicontext

--- 11ContextAPI/src/App.jsx
+++ 11ContextAPI/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
+    <UserContextProvider>
+        <Login />
+        <Profile />
+    </UserContextProvider>  
   )
 }
 
